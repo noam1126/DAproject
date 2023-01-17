e7ee396a17d4b07a7cbd61dd810771f57924fcf4
Noam Nachshon <noam1126@gmail.com>
2023-01-17 14:47:56 +0200

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,27 +1,56 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, make_scorer
+
+
+def split_to_train_and_test(dataset, test_ratio, rand_state):
+    X = dataset.drop(columns="Score", axis=1)
+    y = dataset["Score"]
+    X_train, X_test, y_train, y_test = train_test_split(X.iloc[1:], y.iloc[1:], test_size=test_ratio, random_state=rand_state)
+    return X_train, X_test, y_train, y_test
+
+def calc_evaluation_val(eval_metric, y_test, y_predicted):
+    if (eval_metric == 'accuracy'):
+        return metrics.accuracy_score(y_test, y_predicted)
+
+    if (eval_metric == 'precision'):
+        return metrics.precision_score(y_test, y_predicted)
+
+    if (eval_metric == 'f1'):
+        return metrics.f1_score(y_test, y_predicted)
+
+    if (eval_metric == 'recall'):
+        return metrics.recall_score(y_test, y_predicted)
+
+    if (eval_metric == 'confusion_matrix'):
+        return metrics.confusion_matrix(y_test, y_predicted)
+
+def find_best_k_for_KNN(X_train, y_train):
+    n={'n_neighbors':[2]}
+    clf=GridSearchCV(KNeighborsClassifier(),n,scoring=make_scorer(metrics.f1_score,greater_is_better=True))
+    clf.fit(X_train, y_train)
+    best_K=clf.best_params_['n_neighbors']
+    return best_K
+
 
 data = pd.read_csv("C:\develop\DAproject/CleanWineQuality.csv")
 
-X = data.drop("Score", axis=1)
-y = data["Score"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
+df=data.copy()
+X_train, X_test, y_train, y_test= split_to_train_and_test(df, 0.2, 42)
 
-k=5
-knn = KNeighborsClassifier(n_neighbors=k) # specify the value of k
+k=find_best_k_for_KNN(X_train, y_train)
+knn = KNeighborsClassifier(n_neighbors=k)
 knn.fit(X_train, y_train)
 
 y_pred = knn.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = calc_evaluation_val('accuracy',y_test, y_pred)
 print("Accuracy:", accuracy)
 
-#try different value of k,
-#test the accuracy and choose the best one
 
 #Once you have a well-performing model, you can use it to classify new wine samples based on their attributes.
 #for example:
-new_wine = [["Château Cos d'Estournel 2019 G d'Estournel (Médoc)",39,13.5,750,1,"Médoc, Bordeaux, France","Bordeaux-style Red Blend","Château Cos d'Estournel"]]
-prediction = knn.predict(new_wine)
-print(prediction)
+#new_wine = [["Château Cos d'Estournel 2019 G d'Estournel (Médoc)",39,13.5,750,1,"Médoc, Bordeaux, France","Bordeaux-style Red Blend","Château Cos d'Estournel"]]
+#prediction = knn.predict(new_wine)
+#print(prediction)
